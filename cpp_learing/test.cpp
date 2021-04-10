d96ad39e54bdd5ec0cede70f37ad78f95aa11a00
jyf-111 <1920950476@qq.com>
2021-04-10 15:30:43 +0800

    1

--- cpp_learing/test.cpp
+++ cpp_learing/test.cpp
@@ -1,36 +1,99 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class CExample
-{
-private:
-    int a;
-public:
-    //构造函数
-    CExample(int b)
-    {
-        a=b;
-        printf("constructor is called\n");
-    }
-    //拷贝构造函数
-    CExample(const CExample & c)
-    {
-        a=c.a;//是两个对象不同的成员变量a
-        printf("copy constructor is called\n");
-    }
-    //析构函数
-    ~CExample()
-    {
-        cout<<"destructor is called\n";
-    }
-    void Show()
-    {
-        cout<<a<<endl;
-    }
+
+typedef struct TreeNode *Tree;
+struct TreeNode {
+	Tree Left;
+	Tree Right;
+	int data;
+	int height;
 };
-int main()
-{
-    CExample A(100);
-    CExample B=A;
-    B.Show(); 
-    return 0;
+
+Tree NewNode(int t) {
+	Tree T;
+	T = new struct TreeNode;
+	T->Left = T->Right = NULL;
+	T->data = t;
+	T->height = 0;//二叉树中结点的高度即该结点到叶子结点的路径长度
+	return T;
+}
+int Max(int a, int b) {
+	return a > b ? a : b;
+}
+int GetNodeHeight(Tree T) {//判断一个结点的高度：到叶子节点的最短路径
+	if (!T)return -1;
+	else {
+		return T->height;
+	}
+}
+
+Tree SingleLeftRotation(Tree A) {//左单旋 向右转
+	Tree B = A->Left;
+	A->Left = B->Right;//防止忽略B的右子树
+	B->Right = A;
+	//调整结束
+	A->height = Max(GetNodeHeight(A->Left), GetNodeHeight(A->Right))+1;
+	B->height = Max(GetNodeHeight(A), GetNodeHeight(B->Left)) + 1;
+	return B;
+}
+Tree SingleRightRotation(Tree A) {//
+	Tree B = A->Right;
+	A->Right = B->Left;
+	B->Left = A;
+	A->height = Max(GetNodeHeight(A->Left), GetNodeHeight(A->Right)) + 1;
+	B->height = Max(A->height, GetNodeHeight(B->Right)) + 1;
+	return B;
+}
+Tree DoubleLeftRightRotation(Tree A) {
+	//B和C做右单旋，返回C
+	A->Left = SingleRightRotation(A->Left);
+	//A和C做左单旋
+	return SingleLeftRotation(A);
+}
+Tree DoubleRightLeftRotation(Tree A) {
+	//B和C做zuo单旋，返回C
+	A->Right = SingleLeftRotation(A->Right);
+	//A和C做you单旋
+	return SingleRightRotation(A);
+}
+
+Tree insert(Tree T, int t) {
+	if (!T) {
+		T = NewNode(t);//建立一棵树的头结点 
+	}
+	else {
+		if (t > T->data) {//待插入的数据大于当前结点数据，则应该插在其右子树上面
+			T->Right = insert(T->Right, t);
+			if (GetNodeHeight(T->Right) - GetNodeHeight(T->Left) == 2) {
+				if (t > T->Right->data) {
+					T = SingleRightRotation(T);
+				}
+				else T = DoubleRightLeftRotation(T);
+			}
+		}
+		else {
+			T->Left = insert(T->Left, t);
+			if (GetNodeHeight(T->Left) - GetNodeHeight(T->Right) == 2) {
+				if (t < T->Left->data) {
+					T = SingleLeftRotation(T);
+				}
+				else T = DoubleLeftRightRotation(T);
+			}
+		}
+	}
+	T->height = Max(GetNodeHeight(T->Left), GetNodeHeight(T->Right)) + 1;
+	return T;
+}
+int main() {
+	int N,t;
+	cin >> N>>t;
+	Tree T = NewNode(t);
+	for (int i = 1; i < N; i++)
+	{		
+		cin >> t;
+		T = insert(T, t);
+	}
+	if (T)cout << T->data << endl;//T里面有内容认为是真，否则为假
+	system("pause");
+	return 0;
 }
---
+++ date_struct/static_double_side.cpp
@@ -0,0 +1,91 @@
+/*双端队列*/
+#include<iostream>
+#include<cstdlib>
+#define Maxsize 10
+using namespace std;
+using ElementType = int;
+using DStack = struct SNode *;
+
+struct SNode
+{
+     ElementType date[Maxsize];
+     int top[2];
+};
+
+DStack InitDStack()
+{
+    DStack stack = (DStack)malloc(sizeof(struct SNode));
+    stack->top[0] = -1;
+    stack->top[1] = Maxsize;
+    return stack;
+}
+
+int Pop(DStack stack,int i)
+{
+    int num;
+    switch(i)
+    {
+        
+        case 0 :
+            if(stack->top[0]==-1){
+                cout << "stack No 0 is empty,pop fail" << endl;  
+            }else{
+                num = stack->date[(stack->top[0])--];
+            }
+            break;
+        case 1 :
+            if(stack->top[1]==Maxsize){
+                cout << "stack No 1 is empty,pop fail" << endl; 
+            }else{
+                num = stack->date[(stack->top[1])++];
+            }
+            break;
+        default:
+            
+            break;
+    }
+    return num;
+}
+
+void Push(DStack stack,int i,ElementType X)
+{
+    switch(i)
+    {
+        case 0:
+            if(stack->top[0]+1 == stack->top[1]){
+                cout << "the doubel side stack is full,push fail" << endl;
+            }else{
+                stack->date[++(stack->top[0])] = X;
+            }
+            break;
+        case 1:
+            if(stack->top[0] == stack->top[1]-1){
+                cout << "the doubel side stack is full,push fail" << endl;
+            }else{
+                stack->date[--(stack->top[1])] = X;
+            }
+            break;
+        default:
+            break;
+    }
+}
+int main()
+{
+    DStack stack;
+    stack = InitDStack();
+    
+    for(auto i =0;i < Maxsize/2;++i){
+        Push(stack,0,i);
+        Push(stack,1,Maxsize-i);
+    }
+    
+    for(auto i =0;i < Maxsize/2;++i){
+        cout<<Pop(stack,0)<<" ";
+    }
+    cout <<endl;
+    
+    for(auto i =0;i < Maxsize/2;++i){
+        cout<<Pop(stack,1) <<" "; 
+    }
+    return 0;
+}
